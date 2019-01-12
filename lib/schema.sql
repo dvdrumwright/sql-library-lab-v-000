17756467a4b1525b26dcef951ccf5ff2fdd40e03
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-01-12 22:53:07 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -7,4 +7,4 @@ CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER,
-  series_id INTEGER), 
+  series_id INTEGER); 
