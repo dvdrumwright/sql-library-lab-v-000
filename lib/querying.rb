50d6e2c567c98e343e4795a4d38583a7ab0090d1
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-01-13 00:58:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,10 +18,10 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id
     WHERE characters.species = 'human' GROUP BY series.title
-    ORDER BY COUNT(*) DESC
-    LIMIT 1; "
+      ORDER BY COUNT(*) DESC
+        LIMIT 1; "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) FROM books GROUP BY COUNT(books) DESC LIMIT 1; "
 end
